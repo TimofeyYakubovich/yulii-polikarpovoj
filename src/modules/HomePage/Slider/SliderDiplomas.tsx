4519f49bbd9e11a06214310e7ceac9bb7452626d
Timofey <yakuvocixh96@mail.ru>
2026-02-02 17:28:40 +0300

    7 commit

--- src/modules/HomePage/Slider/SliderDiplomas.tsx
+++ src/modules/HomePage/Slider/SliderDiplomas.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+// import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
